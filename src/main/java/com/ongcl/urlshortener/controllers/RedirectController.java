93131ece1992fb55dbeb45c4affb13b2461eb99f
CL <ongchangle03@gmail.com>
2025-09-08 00:45:18 +0800

    Create the redirect controller

---
+++ src/main/java/com/ongcl/urlshortener/controllers/RedirectController.java
@@ -0,0 +1,27 @@
+package com.ongcl.urlshortener.controllers;
+
+import com.ongcl.urlshortener.services.UrlShortenerService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class RedirectController {
+    private final UrlShortenerService urlShortenerService;
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirectToLongUrl(@PathVariable String shortCode) {
+        String longUrl = urlShortenerService.getLongUrl(shortCode);
+
+        System.out.println(longUrl);
+
+        // If the service call was successful, build the redirect response.
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header("Location", longUrl)
+                .build();
+    }
+}

6a067095292a7d2f8a2d861b6ff61e7f7b95b0c4
CL <ongchangle03@gmail.com>
2025-09-07 17:57:48 +0800

    Create UrlMapping entity

---
+++ src/main/java/com/ongcl/urlshortener/entities/UrlMapping.java
@@ -0,0 +1,31 @@
+package com.ongcl.urlshortener.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "url_mapping")
+public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "short_code")
+    private String shortCode;
+
+    @Column(name = "long_url")
+    private String longUrl;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
+}

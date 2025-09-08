ed21641bc34331bd387e802eddf7702ff6784449
CL <ongchangle03@gmail.com>
2025-09-08 13:56:18 +0800

    Create short code validation for redirect request

--- src/main/java/com/ongcl/urlshortener/controllers/RedirectController.java
+++ src/main/java/com/ongcl/urlshortener/controllers/RedirectController.java
@@ -1,20 +1,27 @@
 package com.ongcl.urlshortener.controllers;
 
 import com.ongcl.urlshortener.services.UrlShortenerService;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 @AllArgsConstructor
 public class RedirectController {
     private final UrlShortenerService urlShortenerService;
 
     @GetMapping("/{shortCode}")
-    public ResponseEntity<Void> redirectToLongUrl(@PathVariable String shortCode) {
+    public ResponseEntity<Void> redirectToLongUrl(
+            @PathVariable
+            @Pattern(regexp = "[a-zA-Z0-9]{6}", message = "Short code must be 6 alphanumeric characters")
+            String shortCode
+    ) {
         String longUrl = urlShortenerService.getLongUrl(shortCode);
 
         // If the service call was successful, build the redirect response.
--- src/main/java/com/ongcl/urlshortener/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ongcl/urlshortener/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.ongcl.urlshortener.exceptions;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,26 +10,34 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
         // 404 error code when the resource is not found
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException e) {
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
 
         // build a map of field names with error message for each field error
-        e.getBindingResult().getFieldErrors().forEach((error) -> {
+        ex.getBindingResult().getFieldErrors().forEach((error) -> {
             errors.put(error.getField(), error.getDefaultMessage());
         });
 
         // return 400 status code
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "Invalid input. " + ex.getConstraintViolations().iterator().next().getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
